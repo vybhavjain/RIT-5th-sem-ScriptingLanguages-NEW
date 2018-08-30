ab002689a2048c578ae08390447b0fb7c1bc2684
Vikas NS <nsvikas1998@gmail.com>
2018-08-30 23:57:07 +0530

    Update 1b.py

--- B1/1b.py
+++ B1/1b.py
@@ -1,1 +1,9 @@
+words=dict()
+with open('myfile.txt','rb') as file:
+    for word in (word for line in file for word in line.split()):
+        if word in words:
+            words[word]+=1
+        else:
+            words[word]=1
 
+words=dict(sorted(words.items(),key=lambda x:x[1],reverse=True))
