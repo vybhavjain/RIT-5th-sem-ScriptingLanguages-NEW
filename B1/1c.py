ce10ea91b637c3bda647c7836fddccf961733ab6
Vikas NS <nsvikas1998@gmail.com>
2018-08-31 00:09:11 +0530

    Update 1c.py

--- B1/1c.py
+++ B1/1c.py
@@ -1,1 +1,8 @@
+def cus_rec(i,lis):
+    if i==len(lis)-1:
+        return lis[i]
+    else:
+        from_future=cus_rec(i+1,lis)
+        return  lis[i] if lis[i]>from_future else from_future
 
+print(cus_rec(0,[int(input('Please enter {} the number'.format(i+1))) for i in range(5)]))
